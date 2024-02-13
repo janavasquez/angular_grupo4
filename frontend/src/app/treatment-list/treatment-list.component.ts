ecb3d524caf29101225458a2fec55504d1bca764
ALEIDYS22 <148786566+ALEIDYS22@users.noreply.github.com>
2024-02-13 12:15:45 +0100

    componentes

---
+++ frontend/src/app/treatment-list/treatment-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-treatment-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './treatment-list.component.html',
+  styleUrl: './treatment-list.component.css'
+})
+export class TreatmentListComponent {
+
+}
