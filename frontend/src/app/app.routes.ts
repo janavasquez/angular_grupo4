819c2f5f223714e566a66f34e1fe892bb3d5e9dd
emilia <ealcantaramieses@hotmail.com>
2024-02-23 12:47:40 +0100

    category-list.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { TreatmentListComponent } from './treatment-list/treatment-list.component';
 import { TreatmentDetailComponent } from './treatment-detail/treatment-detail.component';
 import { HomeComponent } from './home/home.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryDetailComponent } from './category-detail/category-detail.component';
 
 export const routes: Routes = [
 
@@ -19,4 +21,12 @@ export const routes: Routes = [
     path: 'treatments/:id/detail',
     component: TreatmentDetailComponent
   },
+  {
+    path: `category`,
+    component: CategoryListComponent
+  },
+  {
+    path: `category/:id`,
+    component: CategoryDetailComponent
+  }
 ];
--- frontend/src/app/category-list/category-list.component.ts
+++ frontend/src/app/category-list/category-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../interfaces/category.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,RouterLink],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
-export class CategoryListComponent {
+export class CategoryListComponent  implements OnInit{
+
+  categories: Category[] = [];
+
+   constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<Category[]>('http://localhost:3000/categories')
+   .subscribe(categories => this.categories = categories);}
 
 }
+
