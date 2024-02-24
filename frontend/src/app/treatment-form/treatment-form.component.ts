8e90103482eb7b17e0910a7a8115b4d3ff3e3f01
Jana Vasquez <148754895+janavasquez@users.noreply.github.com>
2024-02-24 14:05:37 +0100

    Creacion componentes form

---
+++ frontend/src/app/treatment-form/treatment-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-treatment-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './treatment-form.component.html',
+  styleUrl: './treatment-form.component.css'
+})
+export class TreatmentFormComponent {
+
+}
