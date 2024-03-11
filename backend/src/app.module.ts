2c3ffb0dcab11b23103060a71a7193a421bef9c5
RCF <rolandodj83@gmail.com>
2024-03-11 13:55:50 +0100

    Configuracion mysql en nestjs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Treatment } from './treatment/treatment.model';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost', 
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: 'grupo4_backend', // crear esta base de datos en MySQL primero
+      entities: [Treatment],
+      synchronize: true, // generar tablas en base de datos
+      logging: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
