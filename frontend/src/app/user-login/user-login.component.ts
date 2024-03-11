6bfc11591a75542650285e04fb33668203b8a576
RCF <rolandodj83@gmail.com>
2024-03-11 02:29:54 +0100

    Login finalizado_Revisar

--- frontend/src/app/user-login/user-login.component.ts
+++ frontend/src/app/user-login/user-login.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Login } from '../interfaces/login.model';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
 export class UserLoginComponent {
 
+  loginForm = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+constructor(private fb: FormBuilder,
+            private httpClient: HttpClient,
+            private router: Router ){}
+
+save() {
+
+  let login: Login = {
+    email: this.loginForm.get('email')?.value ?? '',
+    password: this.loginForm.get('password')?.value ?? ''
+  }
+
+  let url ='http://localhost:3000/login';
+  this.httpClient.post<Login>(url, login).subscribe(res => {console.log(res);
+  this.router.navigate(['/']);
+  
+  });
+
+
+}
 }
