66b1676109bcf82ce28425ce296c51c920ae713a
emilia12alcantara <ealcantaramieses@hotmail.com>
2024-03-17 23:35:29 +0100

    formulario-category
    empece form-component de Categoty

--- frontend/src/app/category-form/category-form.component.ts
+++ frontend/src/app/category-form/category-form.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Category } from '../interfaces/category.model';
 
 @Component({
   selector: 'app-category-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './category-form.component.html',
   styleUrl: './category-form.component.css'
 })
-export class CategoryFormComponent {
 
-}
+export class CategoryFormComponent{
+
+      categoryForm = new FormGroup({
+        id: new FormControl(),
+        name: new FormControl(''),
+        photoUrl: new FormControl (" "),
+        minAge: new FormControl()
+
+      });
+
+      save(): void{
+        const id = this.categoryForm.get('id')?.value;
+        console.log(id);
+
+        const name = this.categoryForm.get('name')?.value;
+        console.log(name);
+
+        const photoUrl = this.categoryForm.get('photoUrl')?.value;
+        console.log(photoUrl);
+
+        const minAge = this.categoryForm.get('minAge')?.value;
+        console.log(minAge);
+
+
+      }
+
+      constructor(
+        private httpClient: HttpClient,
+        private activatedRoute:  ActivatedRoute,
+        private router: Router
+      ){}
+
+      ngOnInit(): void {
+
+      }
+
+    }
