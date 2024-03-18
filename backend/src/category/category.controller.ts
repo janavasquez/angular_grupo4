86ad363d88fe26ef065575bdc294c86c01f372d2
emilia12alcantara <ealcantaramieses@hotmail.com>
2024-03-18 12:50:13 +0100

    category.model.ts empezado

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,7 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { Category } from './category.model';
 
 @Controller('category')
 
 export class CategoryController {
-    
+    constructor(
+        @InjectRepository(Category) private categoryRepository<Category>
+    ){}
+    @get()
+         FindAll {
+            return this.CategoryRepository.find();
+         }    
+
 }
