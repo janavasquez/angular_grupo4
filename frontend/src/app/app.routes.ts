dc1170bdb2704df810ff3fbc469c852465bc3a47
Jana Vasquez <148754895+janavasquez@users.noreply.github.com>
2024-03-30 19:08:04 +0100

    Solución error en app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,3 @@
-import { Treatment } from './../../../backend/src/treatment/treatment.model';
 import { Routes } from '@angular/router';
 import { TreatmentListComponent } from './treatment-list/treatment-list.component';
 import { TreatmentDetailComponent } from './treatment-detail/treatment-detail.component';
@@ -17,11 +16,8 @@ import { CompanyFormComponent } from './company-form/company-form.component';
 import { BookingListComponent } from './booking-list/booking-list.component';
 import { BookingDetailComponent } from './booking-detail/booking-detail.component';
 import { BookingFormComponent } from './booking-form/booking-form.component';
-<<<<<<< HEAD
 import { UserFormComponent } from './user-form/user-form.component';
-=======
 import { CategoryFormComponent } from './category-form/category-form.component';
->>>>>>> d4fa7953f4870865678c4cbfdf57c3f89ca1e961
 
 export const routes: Routes = [
 
@@ -82,8 +78,6 @@ export const routes: Routes = [
     component: CategoryDetailComponent
   },
   {
-<<<<<<< HEAD
-=======
     path: 'categories/create',
     component:CategoryFormComponent
 
@@ -101,7 +95,6 @@ export const routes: Routes = [
     component: UserDetailComponent
   },
   {
->>>>>>> d4fa7953f4870865678c4cbfdf57c3f89ca1e961
     path: 'login',
     component: UserLoginComponent
   },
