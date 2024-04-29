244fcae543bdcebf071c858b1109729988cee427
Jana Vasquez <148754895+janavasquez@users.noreply.github.com>
2024-04-29 11:56:53 +0200

    Corrección error jwt

--- backend/src/user/jwt.validator.ts
+++ backend/src/user/jwt.validator.ts
@@ -11,7 +11,7 @@ import { DecodedToken } from "./decoded-token.dto";
 export class JwtValidator extends PassportStrategy(Strategy) {
     constructor(@InjectRepository(User) private userRepository: Repository<User>) {
         super({
-            jwtFromRecuest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
             secretOrKey: 'admin'
         });
